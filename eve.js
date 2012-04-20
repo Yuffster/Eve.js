dea411b5b32be56065147b6784b196c33c990aa5
Michelle Steigerwalt <msteigerwalt@gmail.com>
2012-04-19 22:55:33 -0700

    Version number bump.

--- eve.js
+++ eve.js
@@ -1,5 +1,5 @@
 /**
- * Eve.js <evejs.com> - Version: 0.2 (April 15, 2012)
+ * Eve.js <evejs.com> - Version: 0.4 (April 19, 2012)
  *
  *	   A JavaScript meta-framework for scoped event delegation.
  * 
