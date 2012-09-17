598e19e88eb63c9ad5462442dc23bb0e94e3cf3d
Michelle Steigerwalt <msteigerwalt@gmail.com>
2012-09-16 20:15:04 -0700

    Maybe Travis will work now.

---
+++ tests/phantom.js
@@ -0,0 +1,35 @@
+function wait(con, fun, max) {
+	var waiter = setInterval(function() {
+		if(con()) {
+			fun();
+			clearInterval(waiter);
+		}
+	}, 100);
+	setTimeout(function() {
+		clearInterval(waiter);
+	}, max || 5000);
+};
+
+var page = new WebPage();
+
+page.open(require('fs').workingDirectory+"/run_tests.html", function(status){
+    if (status !== "success") {
+        console.log("Unable to access network");
+        phantom.exit();
+    } else {
+        wait(function(){
+            return page.evaluate(function(){
+                return window.Eve_results !== undefined;
+            });
+        }, function(){
+            var failedNum = page.evaluate(function(){
+                return window.Eve_results.failures;
+            });
+			console.log(
+				(failedNum==0) ?
+				"All tests passed." : failedNum+" failures"
+			);
+            phantom.exit((parseInt(failedNum, 10) > 0) ? 1 : 0);
+        });
+    }
+});
