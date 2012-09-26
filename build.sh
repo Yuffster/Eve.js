a01eb1946cf69956d9b88689ae8d4d7bf12ff653
Michelle Steigerwalt <msteigerwalt@gmail.com>
2012-09-25 22:58:53 -0700

    Build script.

---
+++ build.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+#
+# Eve.js Compilation Script <evejs.com>
+#
+# @author: Michelle Steigerwalt <@Yuffster>
+#
+# Usage:
+#
+#     ./build.sh <version>
+#
+# Assuming you don't have any bugs in your code, this file will perform the
+# following actions:
+#
+#     1. Run the unit test suite against src/eve.js
+#     2. Compress eve.js with copyright notice using Google Closure Compiler
+#     3. Re-run the unit test suite against the minified JavaScript file
+#     4. Change package.json to reflect new version number
+#     5. Change the second line of Eve.js to reflect new version number
+#     6. Commit changes to Git
+#     7. Create a new tag with the version number provided
+#     8. Merge master branch with stable branch
+#     9. Push tags and changes through Git
+#
+# If the unit tests fail, the script will attempt to restore the previous
+# version of eve.min.js.
+
+# Ensure that we've received a single argument.
+if [ $# -lt 1 ]; then
+	echo "Usage: $0 <version number>"
+	exit 1
+fi
+
+VERSION=$1
+VERSION_STRING="Eve.js <evejs.com> - v$1 $(date +"%B %d, %Y")"
+
+# Ensure the version number is in the currect format, mainly so we don't end
+# up accidentally tagging something as vv1.3.
+if [[ $1 =~ [^0-9.]|\.\.|\.$ ]]; then
+	echo "Version number can only contain numbers separated by dots."
+	exit 1
+fi
+
+# Second argument is the location of CDNJS.
+if [ $# -eq 2 ]; then
+	CDN="$2ajax/libs/eve.js/"
+	if [ ! -d "$CDN" ]; then
+		echo "Directory doesn't exist: $CDN"
+		exit 1
+	fi
+fi
+
+# keeps track of whether or not we've changed eve.min.js
+TOUCHED=0 
+
+# Checks to see if the last command executed correctly.  If not, we roll back
+# any relevant changes and exit with a failure code.
+function check_status {
+	if [ "$?" -gt 0 ]; then
+		echo "Build failed. Next time, try compiling with fewer bugs."
+		if [ $TOUCHED -eq 1 ]; then
+			echo "Restoring previous version of eve.min.js"
+			git checkout eve.min.js
+		fi
+		exit 1
+	fi
+}
+
+echo "Stashing changes for clean working tree."
+echo "Run 'git stash pop' to restore if something goes wrong."
+git stash --keep-index -q
+
+# Run our tests against the main eve.js file and ensure that they've passed.
+echo "Running tests against src/eve.js..."
+./tests/run_tests.sh
+check_status
+
+# Run the main eve.js file through Google Closure Compiler.
+echo "Building release $VERSION"
+
+# Ready? Let's start messing with eve.min.js...
+rm eve.min.js
+TOUCHED=1
+
+# Write the version number to the top of our new eve.min.js
+echo // $VERSION_STRING >> eve.min.js
+
+# Recompile src/eve.js and save in eve.min.js
+echo "Compiling with Google Closure Compiler"
+curl -s \
+	-d output_format=text \
+	-d compilation_level=SIMPLE_OPTIMIZATIONS \
+	-d output_info=compiled_code \
+	--data-urlencode "js_code@src/eve.js" \
+	http://closure-compiler.appspot.com/compile \
+	>> eve.min.js
+
+# Run the unit tests again against the minified version.
+echo "Running tests against minifed version..."
+./tests/run_tests.sh eve_file=eve.min
+check_status
+
+# We're done testing, so we can write the version number to the main eve.js
+# file and to package.json.
+sed -i "" "2s/.*/ * $VERSION_STRING/" src/eve.js 
+sed -i "" "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" package.json
+
+# PARTY TIME. \o/
+echo "Build succeeded."
+
+echo "Commiting to Git."
+
+# Add the new eve.min.js file and package.json to the repository.
+git add eve.min.js package.json src/eve.js
+git commit -m "Build of version $VERSION"
+
+# Merge master to stable (since we've run all our tests)
+echo "Merging to stable branch."
+git checkout stable
+git merge master
+git checkout master
+
+# Now tag the release and push our changes.
+echo "Tagging v$VERSION release."
+git tag v$VERSION
+git push --tags
+
+echo "Restoring working tree."
+git stash apply -q
+
+# Push to CDNJS
+if [ $CDN ]; then
+	PWD=`pwd`
+	echo "Adding v$VERSION to local fork of CDNJS."
+	mkdir "$CDN$VERSION"
+	cp eve.min.js "$CDN$VERSION"
+	sed -i "" "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" "$CDN""package.json"
+	cd "$CDN"
+	git add -A
+	git commit -m "Updated Eve.js version to $VERSION."
+	git push
+	cd "$PWD"
+fi
+
+echo "Happy v$VERSION release! \o/"
